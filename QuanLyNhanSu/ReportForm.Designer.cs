6ccd2f8116e70611f2c50f9073701376623d9714
ncsyvn98 <sy100198@gmail.com>
2019-05-14 02:06:52 +0700

    report tạm ok

--- QuanLyNhanSu/ReportForm.Designer.cs
+++ QuanLyNhanSu/ReportForm.Designer.cs
@@ -30,15 +30,58 @@
         {
             this.components = new System.ComponentModel.Container();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource4 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource5 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource6 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.GetNhanVien_NoiHocBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc();
             this.getNhanVienNoiHocBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.getNhanVien_NoiHocTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_NoiHocTableAdapters.GetNhanVien_NoiHocTableAdapter();
             this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.GetNhanVien_NoiHocBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu();
+            this.getNhanVienNgonNguBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getNhanVien_NgonNguTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSetNhanVien_NgonNguTableAdapters.GetNhanVien_NgonNguTableAdapter();
+            this.GetNhanVien_NgonNguBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh();
+            this.getNhanVienKhaNangViTinhBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getNhanVien_KhaNangViTinhTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinhTableAdapters.GetNhanVien_KhaNangViTinhTableAdapter();
+            this.GetNhanVien_KhaNangViTinhBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen();
+            this.getNhanVienKhoaHuanLuyenBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getNhanVien_KhoaHuanLuyenTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyenTableAdapters.GetNhanVien_KhoaHuanLuyenTableAdapter();
+            this.GetNhanVien_KhoaHuanLuyenBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_CongTy();
+            this.getNhanVienCongTyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getNhanVien_CongTyTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien_CongTyTableAdapters.GetNhanVien_CongTyTableAdapter();
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVien();
+            this.getNhanVienBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.getNhanVienTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet_NhanVienTableAdapters.GetNhanVienTableAdapter();
+            this.GetNhanVien_CongTyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NoiHocBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.getNhanVienNoiHocBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NoiHocBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienNgonNguBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NgonNguBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienKhaNangViTinhBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_KhaNangViTinhBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienKhoaHuanLuyenBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_KhoaHuanLuyenBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienCongTyBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_CongTyBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // GetNhanVien_NoiHocBindingSource
+            // 
+            this.GetNhanVien_NoiHocBindingSource.DataMember = "GetNhanVien_NoiHoc";
+            this.GetNhanVien_NoiHocBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc;
             // 
             // n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc
             // 
@@ -58,31 +101,145 @@
             // 
             reportDataSource1.Name = "DataSet_Report_NhanVien_NoiHoc";
             reportDataSource1.Value = this.GetNhanVien_NoiHocBindingSource;
+            reportDataSource2.Name = "DataSet_Report_NhanVien_NgonNgu";
+            reportDataSource2.Value = this.GetNhanVien_NgonNguBindingSource;
+            reportDataSource3.Name = "DataSet_Report_NhanVien_KhaNangViTinh";
+            reportDataSource3.Value = this.GetNhanVien_KhaNangViTinhBindingSource;
+            reportDataSource4.Name = "DataSet_Report_NhanVien_KhoaHuanLuyen";
+            reportDataSource4.Value = this.GetNhanVien_KhoaHuanLuyenBindingSource;
+            reportDataSource5.Name = "DataSet_Report_NhanVien_CongTy";
+            reportDataSource5.Value = this.GetNhanVien_CongTyBindingSource;
+            reportDataSource6.Name = "DataSet_Report_NhanVien";
+            reportDataSource6.Value = this.getNhanVienBindingSource;
             this.reportViewer2.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource4);
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource5);
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource6);
             this.reportViewer2.LocalReport.ReportEmbeddedResource = "QuanLyNhanSu.Report.rdlc";
             this.reportViewer2.Location = new System.Drawing.Point(12, 24);
             this.reportViewer2.Name = "reportViewer2";
             this.reportViewer2.ServerReport.BearerToken = null;
-            this.reportViewer2.Size = new System.Drawing.Size(752, 692);
+            this.reportViewer2.Size = new System.Drawing.Size(820, 692);
             this.reportViewer2.TabIndex = 1;
             // 
-            // GetNhanVien_NoiHocBindingSource
+            // n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu
             // 
-            this.GetNhanVien_NoiHocBindingSource.DataMember = "GetNhanVien_NoiHoc";
-            this.GetNhanVien_NoiHocBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc;
+            this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu.DataSetName = "N05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu";
+            this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getNhanVienNgonNguBindingSource
+            // 
+            this.getNhanVienNgonNguBindingSource.DataMember = "GetNhanVien_NgonNgu";
+            this.getNhanVienNgonNguBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu;
+            // 
+            // getNhanVien_NgonNguTableAdapter
+            // 
+            this.getNhanVien_NgonNguTableAdapter.ClearBeforeFill = true;
+            // 
+            // GetNhanVien_NgonNguBindingSource
+            // 
+            this.GetNhanVien_NgonNguBindingSource.DataMember = "GetNhanVien_NgonNgu";
+            this.GetNhanVien_NgonNguBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu;
+            // 
+            // n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh
+            // 
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh.DataSetName = "N05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh";
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getNhanVienKhaNangViTinhBindingSource
+            // 
+            this.getNhanVienKhaNangViTinhBindingSource.DataMember = "GetNhanVien_KhaNangViTinh";
+            this.getNhanVienKhaNangViTinhBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh;
+            // 
+            // getNhanVien_KhaNangViTinhTableAdapter
+            // 
+            this.getNhanVien_KhaNangViTinhTableAdapter.ClearBeforeFill = true;
+            // 
+            // GetNhanVien_KhaNangViTinhBindingSource
+            // 
+            this.GetNhanVien_KhaNangViTinhBindingSource.DataMember = "GetNhanVien_KhaNangViTinh";
+            this.GetNhanVien_KhaNangViTinhBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh;
+            // 
+            // n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen
+            // 
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen.DataSetName = "N05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen";
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getNhanVienKhoaHuanLuyenBindingSource
+            // 
+            this.getNhanVienKhoaHuanLuyenBindingSource.DataMember = "GetNhanVien_KhoaHuanLuyen";
+            this.getNhanVienKhoaHuanLuyenBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen;
+            // 
+            // getNhanVien_KhoaHuanLuyenTableAdapter
+            // 
+            this.getNhanVien_KhoaHuanLuyenTableAdapter.ClearBeforeFill = true;
+            // 
+            // GetNhanVien_KhoaHuanLuyenBindingSource
+            // 
+            this.GetNhanVien_KhoaHuanLuyenBindingSource.DataMember = "GetNhanVien_KhoaHuanLuyen";
+            this.GetNhanVien_KhoaHuanLuyenBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen;
+            // 
+            // n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy
+            // 
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy.DataSetName = "N05_Ql_NhanSu_T5DataSet_NhanVien_CongTy";
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getNhanVienCongTyBindingSource
+            // 
+            this.getNhanVienCongTyBindingSource.DataMember = "GetNhanVien_CongTy";
+            this.getNhanVienCongTyBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy;
+            // 
+            // getNhanVien_CongTyTableAdapter
+            // 
+            this.getNhanVien_CongTyTableAdapter.ClearBeforeFill = true;
+            // 
+            // n05_Ql_NhanSu_T5DataSet_NhanVien
+            // 
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien.DataSetName = "N05_Ql_NhanSu_T5DataSet_NhanVien";
+            this.n05_Ql_NhanSu_T5DataSet_NhanVien.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // getNhanVienBindingSource
+            // 
+            this.getNhanVienBindingSource.DataMember = "GetNhanVien";
+            this.getNhanVienBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien;
+            // 
+            // getNhanVienTableAdapter
+            // 
+            this.getNhanVienTableAdapter.ClearBeforeFill = true;
+            // 
+            // GetNhanVien_CongTyBindingSource
+            // 
+            this.GetNhanVien_CongTyBindingSource.DataMember = "GetNhanVien_CongTy";
+            this.GetNhanVien_CongTyBindingSource.DataSource = this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy;
             // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 728);
+            this.ClientSize = new System.Drawing.Size(844, 728);
             this.Controls.Add(this.reportViewer2);
             this.Name = "ReportForm";
             this.Text = "ReportForm";
             this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NoiHocBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.getNhanVienNoiHocBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NoiHocBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienNgonNguBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_NgonNguBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienKhaNangViTinhBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_KhaNangViTinhBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienKhoaHuanLuyenBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_KhoaHuanLuyenBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienCongTyBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.n05_Ql_NhanSu_T5DataSet_NhanVien)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getNhanVienBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GetNhanVien_CongTyBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -93,5 +250,24 @@
         private N05_Ql_NhanSu_T5DataSet_NhanVien_NoiHocTableAdapters.GetNhanVien_NoiHocTableAdapter getNhanVien_NoiHocTableAdapter;
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
         private System.Windows.Forms.BindingSource GetNhanVien_NoiHocBindingSource;
+        private N05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu;
+        private System.Windows.Forms.BindingSource getNhanVienNgonNguBindingSource;
+        private N05_Ql_NhanSu_T5DataSetNhanVien_NgonNguTableAdapters.GetNhanVien_NgonNguTableAdapter getNhanVien_NgonNguTableAdapter;
+        private System.Windows.Forms.BindingSource GetNhanVien_NgonNguBindingSource;
+        private System.Windows.Forms.BindingSource GetNhanVien_KhaNangViTinhBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh;
+        private System.Windows.Forms.BindingSource getNhanVienKhaNangViTinhBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinhTableAdapters.GetNhanVien_KhaNangViTinhTableAdapter getNhanVien_KhaNangViTinhTableAdapter;
+        private System.Windows.Forms.BindingSource GetNhanVien_KhoaHuanLuyenBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen;
+        private System.Windows.Forms.BindingSource getNhanVienKhoaHuanLuyenBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyenTableAdapters.GetNhanVien_KhoaHuanLuyenTableAdapter getNhanVien_KhoaHuanLuyenTableAdapter;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_CongTy n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy;
+        private System.Windows.Forms.BindingSource getNhanVienCongTyBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien_CongTyTableAdapters.GetNhanVien_CongTyTableAdapter getNhanVien_CongTyTableAdapter;
+        private N05_Ql_NhanSu_T5DataSet_NhanVien n05_Ql_NhanSu_T5DataSet_NhanVien;
+        private System.Windows.Forms.BindingSource getNhanVienBindingSource;
+        private N05_Ql_NhanSu_T5DataSet_NhanVienTableAdapters.GetNhanVienTableAdapter getNhanVienTableAdapter;
+        private System.Windows.Forms.BindingSource GetNhanVien_CongTyBindingSource;
     }
 }
--- QuanLyNhanSu/ReportForm.cs
+++ QuanLyNhanSu/ReportForm.cs
@@ -19,6 +19,21 @@ namespace QuanLyNhanSu
 
         private void ReportForm_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy.GetNhanVien_CongTy' table. You can move, or remove it, as needed.
+            this.getNhanVien_CongTyTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSet_NhanVien_CongTy.GetNhanVien_CongTy, 16150151);
+
+            // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSet_NhanVien.GetNhanVien' table. You can move, or remove it, as needed.
+            this.getNhanVienTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSet_NhanVien.GetNhanVien, 16150151);
+
+            // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen.GetNhanVien_KhoaHuanLuyen' table. You can move, or remove it, as needed.
+            this.getNhanVien_KhoaHuanLuyenTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhoaHuanLuyen.GetNhanVien_KhoaHuanLuyen, 16150151);
+
+            // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh.GetNhanVien_KhaNangViTinh' table. You can move, or remove it, as needed.
+            this.getNhanVien_KhaNangViTinhTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSet_NhanVien_KhaNangViTinh.GetNhanVien_KhaNangViTinh, 16150151);
+
+            // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu.GetNhanVien_NgonNgu' table. You can move, or remove it, as needed.
+            this.getNhanVien_NgonNguTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSetNhanVien_NgonNgu.GetNhanVien_NgonNgu, 16150151);
+
             // TODO: This line of code loads data into the 'n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc.GetNhanVien_NoiHoc' table. You can move, or remove it, as needed.
             this.getNhanVien_NoiHocTableAdapter.Fill(this.n05_Ql_NhanSu_T5DataSet_NhanVien_NoiHoc.GetNhanVien_NoiHoc, 16150151);
 
