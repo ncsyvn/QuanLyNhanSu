617b8e120c19a36b7d3fd83dbd4e7f00d4b4b354
ncsyvn98 <sy100198@gmail.com>
2019-05-14 14:08:38 +0700

    fail

--- QuanLyNhanSu/NghiepVu.Designer.cs
+++ QuanLyNhanSu/NghiepVu.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panelNghiepVu = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonExport = new System.Windows.Forms.Button();
             this.panelNhanVien_NgonNgu = new System.Windows.Forms.Panel();
             this.buttonNhanVien_NgonNgu = new System.Windows.Forms.Button();
             this.dataGridViewNhanVien_NgonNgu = new System.Windows.Forms.DataGridView();
@@ -297,6 +297,11 @@
             this.noiHocTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet8TableAdapters.NoiHocTableAdapter();
             this.khaNangViTinhTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet9TableAdapters.KhaNangViTinhTableAdapter();
             this.ngonNguTableAdapter = new QuanLyNhanSu.N05_Ql_NhanSu_T5DataSet10TableAdapters.NgonNguTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.panelNghiepVu.SuspendLayout();
             this.panelNhanVien_NgonNgu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNhanVien_NgonNgu)).BeginInit();
@@ -341,7 +346,12 @@
             // 
             this.panelNghiepVu.AutoScroll = true;
             this.panelNghiepVu.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panelNghiepVu.Controls.Add(this.button1);
+            this.panelNghiepVu.Controls.Add(this.label6);
+            this.panelNghiepVu.Controls.Add(this.label5);
+            this.panelNghiepVu.Controls.Add(this.label4);
+            this.panelNghiepVu.Controls.Add(this.label2);
+            this.panelNghiepVu.Controls.Add(this.label1);
+            this.panelNghiepVu.Controls.Add(this.buttonExport);
             this.panelNghiepVu.Controls.Add(this.panelNhanVien_NgonNgu);
             this.panelNghiepVu.Controls.Add(this.panelNhanVien_CongTy);
             this.panelNghiepVu.Controls.Add(this.panelNhanVien_KHL);
@@ -352,14 +362,15 @@
             this.panelNghiepVu.Size = new System.Drawing.Size(1358, 738);
             this.panelNghiepVu.TabIndex = 0;
             // 
-            // button1
+            // buttonExport
             // 
-            this.button1.Location = new System.Drawing.Point(1276, 36);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonExport.BackgroundImage = global::QuanLyNhanSu.Properties.Resources.export_flat;
+            this.buttonExport.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonExport.Location = new System.Drawing.Point(1272, 268);
+            this.buttonExport.Name = "buttonExport";
+            this.buttonExport.Size = new System.Drawing.Size(75, 69);
+            this.buttonExport.TabIndex = 3;
+            this.buttonExport.UseVisualStyleBackColor = true;
             // 
             // panelNhanVien_NgonNgu
             // 
@@ -2782,6 +2793,60 @@
             // 
             this.ngonNguTableAdapter.ClearBeforeFill = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(1278, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Quản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(1290, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lý";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(1278, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Năng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Unicode MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Crimson;
+            this.label5.Location = new System.Drawing.Point(1287, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Lực";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1286, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Export";
+            // 
             // NghiepVu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -2794,6 +2859,7 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NghiepVu_FormClosing);
             this.Load += new System.EventHandler(this.NghiepVu_Load);
             this.panelNghiepVu.ResumeLayout(false);
+            this.panelNghiepVu.PerformLayout();
             this.panelNhanVien_NgonNgu.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNhanVien_NgonNgu)).EndInit();
             this.panelTimNhanVien_NgonNgu.ResumeLayout(false);
@@ -2853,7 +2919,7 @@
         #endregion
 
         private System.Windows.Forms.Panel panelNghiepVu;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonExport;
         private System.Windows.Forms.Panel panelNhanVien_NgonNgu;
         private System.Windows.Forms.Panel panelNhanVien_CongTy;
         private System.Windows.Forms.Panel panelNhanVien_KHL;
@@ -3120,5 +3186,10 @@
         private N05_Ql_NhanSu_T5DataSet10 n05_Ql_NhanSu_T5DataSet10;
         private System.Windows.Forms.BindingSource ngonNguBindingSource;
         private N05_Ql_NhanSu_T5DataSet10TableAdapters.NgonNguTableAdapter ngonNguTableAdapter;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
     }
 }
---
+++ QuanLyNhanSu/StartForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace QuanLyNhanSu
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonDanhMuc = new System.Windows.Forms.Button();
+            this.buttonNghiepVu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonDanhMuc
+            // 
+            this.buttonDanhMuc.Location = new System.Drawing.Point(187, 109);
+            this.buttonDanhMuc.Name = "buttonDanhMuc";
+            this.buttonDanhMuc.Size = new System.Drawing.Size(75, 77);
+            this.buttonDanhMuc.TabIndex = 0;
+            this.buttonDanhMuc.UseVisualStyleBackColor = true;
+            this.buttonDanhMuc.Click += new System.EventHandler(this.buttonDanhMuc_Click);
+            // 
+            // buttonNghiepVu
+            // 
+            this.buttonNghiepVu.Location = new System.Drawing.Point(305, 109);
+            this.buttonNghiepVu.Name = "buttonNghiepVu";
+            this.buttonNghiepVu.Size = new System.Drawing.Size(75, 77);
+            this.buttonNghiepVu.TabIndex = 1;
+            this.buttonNghiepVu.UseVisualStyleBackColor = true;
+            this.buttonNghiepVu.Click += new System.EventHandler(this.buttonNghiepVu_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(168, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "QUẢN LÝ NHÂN SỰ";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNghiepVu);
+            this.Controls.Add(this.buttonDanhMuc);
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonDanhMuc;
+        private System.Windows.Forms.Button buttonNghiepVu;
+        private System.Windows.Forms.Label label1;
+    }
+}
